a26b1942459c9a45d5c55bb3b714485ab52311ea
Gurdyal Singh <102032512+Gurdyal006@users.noreply.github.com>
2023-12-13 18:38:09 +0530

    show conflict

--- test2.js
+++ test2.js
@@ -1,4 +1,4 @@
 const data = "This is feat1 branch";
-const feat3 = "This is feat3 (master) branch solve conflict check data";
+const feat3 = "This is feat3  branch solve conflict check data";
 
 console.log(`${data} ${feat3}`);
